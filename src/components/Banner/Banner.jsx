9e0a4e2b7784951b8c41dab021cd4bcce4b368a9
LishadWahid <lishadwahid@gmail.com>
2025-10-06 12:37:33 +0300

    added all update

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,13 +3,13 @@ import bookImage from '../../assets/books.jpg'
 
 const Banner = () => {
     return (
-        <div className='border-2 border-green-500 max-w-[1140px] h-[554px] bg-[#1313130d] flex justify-around items-center p-5'>
-           <div className='flex flex-col  gap-8 items-start p-20'>
+        <div className='max-w-[1140px] md:h-[554px] mx-auto h-auto bg-[#1313130d] flex flex-col md:flex-row justify-around items-center p-5'>
+           <div className='flex flex-col  gap-6 items-start p-20'>
                 <h1 className='text-5xl'>Books to freshen up your bookshelf</h1>
-                <button className='btn btn-primary p-5'>View The List</button>
+                <button className='btn btn-primary px-6 py-3 text-base md:text-lg'>View The List</button>
             </div>
             <div>
-                <img className='pr-20' src={bookImage} alt="" />
+                <img className='w-full max-w-xs md:max-w-sm md:pr-20 lg:pr-20' src={bookImage} alt="" />
             </div> 
         </div>
     );
